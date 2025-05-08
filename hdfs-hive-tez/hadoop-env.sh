dbd9467331ad749d509516e5bb61b63268afa13b
ngoupatrick <ngounepatrick@gmail.com>
2025-05-08 23:10:55 +0000

    hdfs hive tez v2

--- hdfs-hive-tez/hadoop-env.sh
+++ hdfs-hive-tez/hadoop-env.sh
@@ -1,4 +1,5 @@
-export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+#export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export LANG=en_US.UTF-8
 export HADOOP_OS_TYPE=${HADOOP_OS_TYPE:-$(uname -s)}
 export HADOOP_LOG_DIR=~/logs
